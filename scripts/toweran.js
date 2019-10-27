1828d63d0badfd499666f053d8f63c5672c541f5
Alexandr Shevchenko <ls.shov@gmail.com>
2019-10-27 12:32:28 +0300

    Feature/17 log msg toweran script chalk (#3)
    * 17: restructured, tested
    
    * 17: finished

--- scripts/toweran.js
+++ scripts/toweran.js
@@ -10,117 +10,194 @@
 const path = require('path')
 const fs = require('fs-extra')
 const glob = require('glob')
+const chalk = require('chalk')
+
+const symbol = {
+  v: '✓',
+  i: 'i',
+  x: '✗'
+}
+
+const moduleDir = path.resolve(__dirname + '/..')
 
 try {
 
+  //Process the input
+  const [, , ...incomingArgs] = process.argv
+
+  /**
+   * Set options
+   */
+  const opts = {
+    travisFixtures: false
+  }
+
+  /**
+   * Set arguments
+   */
+  const args = []
+
+  incomingArgs.forEach(val => {
+    switch (true) {
+
+      case '--help' === val:
+        printHelp()
+        process.exit(0)
+        break
+
+      case /^--.+$/.test(val):
+        opts[argToOption(val)] = true
+        break
+
+      default:
+        args.push(val)
+    }
+  })
+
+  /**
+   * As a default
+   * @type {function}
+   */
+  let action = printHelp
+
   //Process arguments
-  const [, , ...args] = process.argv
-  const options = {}
+  if (!args[0]) {
+    throw new Error(chalk`{red ${symbol.x}} Unknown action, run: \n\t$ npx toweran --help\nto get more information`)
+  }
 
-  switch (true) {
-    case !args[0]:
-      throw new Error(`unknown action, run \n$ npx toweran --help\nto get more information`)
-
-    case 'create-project' === args[0]:
-
-      if ('--travis-fixtures' === args[2]) {
-        options.travisFixtures = true
-      }
-
-      break
-    case '--help' === args[0]:
-      console.log(`To create a project structure from the boilerplate run\n$ npx toweran create-project [project path]`)
-      process.exit(0)
+  //Choose the action
+  switch (args[0]) {
+    case 'create-project':
+      action = createProject
+      console.info(chalk`{green ${symbol.v}} Creating a project...`)
       break
 
     default:
-      throw new Error(`create-project is only one supported action for now! \n$ npx toweran create-project [project path]`)
+      throw new Error(chalk`{red ${symbol.x}} create-project is only one supported action for now! \n\t$ npx toweran create-project [project path]`)
   }
 
-  args[1] = args[1] || ''
+  //Call the action
+  const restOfArgs = args.slice(1, args.length)
+  action.apply({opts}, restOfArgs)
 
-
-  //Paths
-  const projectDir = path.resolve(`${process.cwd()}/${args[1]}`)
-
-  try {
-    fs.ensureDirSync(projectDir, 0o2775)
-  } catch (e) {
-    throw new Error(`Given path '${projectDir}' is exists and is not a directory!`)
-  }
-
-  const moduleDir = path.resolve(__dirname + '/..')
-
-    //Copy boilerplate
-  ;(() => {
-    const targetDirs = []
-
-    glob.sync(`${moduleDir}/boilerplate/*`).forEach(file => {
-      const fileName = file.replace(/(.*)\/boilerplate\/([^\/]*)$/i, '$2')
-
-      if (fs.lstatSync(file).isDirectory()) {
-        targetDirs.push(fileName)
-      }
-
-      fs.copySync(file, `${projectDir}/${fileName}`)
-    })
-
-    fs.copySync(`${moduleDir}/boilerplate/.gitignore.content`, `${projectDir}/.gitignore`)
-
-    fs.copySync(`${moduleDir}/boilerplate/.env-example`, `${projectDir}/.env-example`)
-    fs.copySync(`${moduleDir}/boilerplate/.env-example`, `${projectDir}/.env`)
-
-    const gitKeepExpression = new RegExp(`${projectDir}\/(${targetDirs.join('|')})\/(.*\/)?\.gitkeep$`, 'i')
-
-    glob.sync(`${projectDir}/**/.gitkeep`).forEach(file => {
-      if (gitKeepExpression.test(file)) {
-        fs.removeSync(file)
-      }
-    })
-
-    let bootstrapFileContent = fs.readFileSync(`${projectDir}/bootstrap.js`, 'utf-8')
-    bootstrapFileContent = bootstrapFileContent.replace(`require('../toweran')`, `require('toweran')`)
-    fs.writeFileSync(`${projectDir}/bootstrap.js`, bootstrapFileContent, 'utf8')
-
-    if (fs.existsSync(`${projectDir}/package.json`)) {
-      try {
-        let packageJsonContent = fs.readFileSync(`${projectDir}/package.json`, 'utf-8')
-        packageJsonContent = JSON.parse(packageJsonContent)
-        packageJsonContent.jest = {
-          "testRegex": "/tests/.*Test.js$",
-          "rootDir": "."
-        }
-
-        if (!packageJsonContent.scripts) {
-          packageJsonContent.scripts = {}
-        }
-
-        packageJsonContent.scripts.test = "jest --runInBand"
-
-        //Push dependencies manually for travis test environment
-        if(options.travisFixtures) {
-          let modulePackageJson = fs.readFileSync(`${moduleDir}/package.json`, 'utf-8')
-          modulePackageJson = JSON.parse(modulePackageJson)
-
-          packageJsonContent.dependencies = modulePackageJson.dependencies
-          packageJsonContent.dependencies.toweran = 'file:/..'
-
-          packageJsonContent.devDependencies = modulePackageJson.devDependencies
-        }
-
-        packageJsonContent = JSON.stringify(packageJsonContent, null, 2) + '\n'
-
-        fs.writeFileSync(`${projectDir}/package.json`, packageJsonContent, 'utf-8')
-
-      } catch (e) {
-        console.warn(`x package.json is found, but can't update!`)
-      }
-    }
-
-    console.log(`🏰 Done`)
-  })()
 
 } catch (e) {
   console.error(e.message)
   process.exit(1)
+}
+
+/**
+ * Create project action, roll-out the boilerplate in the given path
+ * @param {string} targetPath
+ */
+function createProject(targetPath = '') {
+  //Paths
+  const projectDir = path.resolve(`${process.cwd()}/${targetPath}`)
+
+  try {
+    fs.ensureDirSync(projectDir, 0o2775)
+  } catch (e) {
+    throw new Error(chalk`{red ${symbol.x}} Given path '${projectDir}' exists and is not a directory!`)
+  }
+
+  console.info(chalk`{green ${symbol.v}} The path is good-to-go: ${projectDir}`)
+
+  const targetDirs = []
+
+  glob.sync(`${moduleDir}/boilerplate/*`).forEach(file => {
+    const fileName = file.replace(/(.*)\/boilerplate\/([^\/]*)$/i, '$2')
+
+    if (fs.lstatSync(file).isDirectory()) {
+      targetDirs.push(fileName)
+    }
+
+    fs.copySync(file, `${projectDir}/${fileName}`)
+  })
+
+  fs.copySync(`${moduleDir}/boilerplate/.gitignore.content`, `${projectDir}/.gitignore`)
+
+  fs.copySync(`${moduleDir}/boilerplate/.env-example`, `${projectDir}/.env-example`)
+  fs.copySync(`${moduleDir}/boilerplate/.env-example`, `${projectDir}/.env`)
+
+  console.info(chalk`{green ${symbol.v}} The boilerplate has been copied`)
+
+  const gitKeepExpression = new RegExp(`${projectDir}\/(${targetDirs.join('|')})\/(.*\/)?\.gitkeep$`, 'i')
+
+  glob.sync(`${projectDir}/**/.gitkeep`).forEach(file => {
+    if (gitKeepExpression.test(file)) {
+      fs.removeSync(file)
+    }
+  })
+
+  console.info(chalk`{green ${symbol.v}} Clean up is done`)
+
+  let bootstrapFileContent = fs.readFileSync(`${projectDir}/bootstrap.js`, 'utf-8')
+  bootstrapFileContent = bootstrapFileContent.replace(`require('../toweran')`, `require('toweran')`)
+  fs.writeFileSync(`${projectDir}/bootstrap.js`, bootstrapFileContent, 'utf8')
+
+  console.info(chalk`{green ${symbol.v}} Toweran dependence set as an module`)
+
+  if (fs.existsSync(`${projectDir}/package.json`)) {
+    console.info(chalk`{yellow ${symbol.i}} package.json has been detected`)
+
+    try {
+      let packageJsonContent = fs.readFileSync(`${projectDir}/package.json`, 'utf-8')
+      packageJsonContent = JSON.parse(packageJsonContent)
+      packageJsonContent.jest = {
+        "testRegex": "/tests/.*Test.js$",
+        "rootDir": "."
+      }
+
+      if (!packageJsonContent.scripts) {
+        packageJsonContent.scripts = {}
+      }
+
+      packageJsonContent.scripts.test = "jest --runInBand"
+
+      //Push dependencies manually for travis test environment
+      if (this.opts.travisFixtures) {
+        console.info(chalk`{yellow ${symbol.i}} Travis fixtures are on`)
+
+        let modulePackageJson = fs.readFileSync(`${moduleDir}/package.json`, 'utf-8')
+        modulePackageJson = JSON.parse(modulePackageJson)
+
+        packageJsonContent.dependencies = modulePackageJson.dependencies
+        packageJsonContent.dependencies.toweran = 'file:/..'
+
+        packageJsonContent.devDependencies = modulePackageJson.devDependencies
+      }
+
+      packageJsonContent = JSON.stringify(packageJsonContent, null, 2) + '\n'
+
+      fs.writeFileSync(`${projectDir}/package.json`, packageJsonContent, 'utf-8')
+
+      console.info(chalk`{green ${symbol.v}} package.json has been updated`)
+
+    } catch (e) {
+      console.warn(chalk`{orange ${symbol.x}} Can't update package.json, skip`)
+    }
+  }
+
+  console.log(`🏰 Done`)
+}
+
+/**
+ * Print the help
+ */
+function printHelp() {
+  console.log(chalk`{yellow ${symbol.i}} To create a project structure from the boilerplate run:\n\t$ npx toweran create-project [project path]`)
+}
+
+/**
+ * A helper to convert options to object keys
+ * @param str
+ * @return {string}
+ */
+function argToOption(str) {
+  return str.replace(/^--(.*)$/, '$1').replace(
+    /([-_][a-z])/g,
+    (group) => group.toUpperCase()
+      .replace('-', '')
+      .replace('_', '')
+  )
 }
