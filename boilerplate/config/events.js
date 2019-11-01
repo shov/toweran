e680b3deda2c5a43248ce021524d2abda03ae67d
Mehran Hadidi <mehran.hadidi@gmail.com>
2019-11-01 18:07:01 +0300

    20: add config file to store events & associated listeners

---
+++ boilerplate/config/events.js
@@ -0,0 +1,13 @@
+module.exports = {
+    /**
+     * A collection of events & associated listeners
+     */
+    events: [
+        {
+            event: 'start_application',
+            listeners: [
+                // new (require('../listeners/SampleListener'))
+            ],
+        },
+    ],
+}
