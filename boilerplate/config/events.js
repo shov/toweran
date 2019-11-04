00343acb6669f7cfc58f128b1662312423df0236
Alexandr Shevchenko <ls.shov@gmail.com>
2019-11-04 21:58:46 +0100

    20: added fixes, got back cond to off config files inc, reformated, added todos

--- boilerplate/config/events.js
+++ boilerplate/config/events.js
@@ -1,13 +1,15 @@
 module.exports = {
-    /**
-     * A collection of events & associated listeners
-     */
-    events: [
-        {
-            event: 'start_application',
-            listeners: [
-                // new (require('../listeners/SampleListener'))
-            ],
-        },
-    ],
+  /**
+   * A collection of events & associated listeners
+   */
+  events: [
+    {
+      event: 'start_application',
+      listeners: [
+        //TODO: probably we better set dot notation hee and use container?
+        //TODO: and it would be great if we have a possibility to use callbacks here
+        // new (require('../listeners/SampleListener'))
+      ],
+    },
+  ],
 }
