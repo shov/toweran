63cc731577031f118973c4f544e2a7098eca7294
Mehran Hadidi <mehran.hadidi@gmail.com>
2019-11-06 19:03:11 +0300

    20: add unit tests for EventManager

---
+++ tests/unit/lib/EventManagerTest.js
@@ -0,0 +1,78 @@
+'use strict'
+
+require('../../bootstrap')
+const EventManager = require(toweran.FRAMEWORK_PATH + '/lib/EventManager')
+
+describe(`Event Manager unit tests`, () => {
+
+    const InvalidArgumentException = toweran.InvalidArgumentException
+
+    it(`should not created without passing config object`, () => {
+        const act = () => {
+            const eventManager = new EventManager()
+        }
+
+        expect(act).toThrow(InvalidArgumentException)
+    })
+
+    it(`should not created without config file`, () => {
+        const act = () => {
+            const eventManager = new EventManager({})
+        }
+
+        expect(act).not.toThrow(InvalidArgumentException)
+    })
+
+    it(`"exists" method should return true if event exists`, () => {
+        const eventManager = new EventManager({
+            events: { events: [{ event: 'eventName', listeners: [] }] }
+        })
+
+        eventManager.init()
+
+        const exists = eventManager.exists('eventName')
+
+        expect(exists).toBe(true)
+    })
+
+    it(`"exists" method should return false if event not exists`, () => {
+        const eventManager = new EventManager({
+            events: { events: [{ event: 'eventName', listeners: [] }] }
+        })
+
+        eventManager.init()
+
+        const exists = eventManager.exists('undefinedEvent')
+
+        expect(exists).toBe(false)
+    })
+
+    it(`"should not be able to run before init`, () => {
+        const act = () => {
+            const eventManager = new EventManager({
+                events: { events: [{ event: 'eventName', listeners: [] }] }
+            })
+
+            eventManager.dispatch('eventName')
+        }
+
+        expect(act).toThrow(Error)
+    })
+
+    it(`"should be functional after initialization`, () => {
+        const act = () => {
+            const eventManager = new EventManager({
+                events: { events: [{ event: 'eventName', listeners: [] }] }
+            })
+
+            eventManager.init()
+
+            eventManager.dispatch('eventName')
+        }
+
+        !expect(act).not.toThrow(Error)
+    })
+
+    // TODO should contains init, dispatch
+    // TODO tests for dispatching and listeners
+})
