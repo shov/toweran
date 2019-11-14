d4c3f7f950d0beb465944163d9accc9c3a2a79ac
Mehran Hadidi <mehran.hadidi@gmail.com>
2019-11-14 15:00:55 +0300

    20: move feature test to unit test file

--- tests/unit/lib/EventManagerTest.js
+++ tests/unit/lib/EventManagerTest.js
@@ -2,6 +2,34 @@
 
 require('../../bootstrap')
 const EventManager = require(toweran.FRAMEWORK_PATH + '/lib/EventManager')
+const ListenerInterface = toweran.ListenerInterface
+
+let listenerCalled = false
+let listenerPayload = {}
+
+/**
+ * Mocked Listener
+ * @type {ListenerInterface}
+ */
+const MockedListener = class extends ListenerInterface {
+    constructor() {
+        super()
+    }
+
+    handle(payload) {
+        listenerCalled = true
+        listenerPayload = payload
+    }
+}
+
+/**
+ * This is a sample payload
+ * @type {{Object}}
+ */
+const mockedPayload = {
+    name: 'test',
+    email: 'test@test.com',
+}
 
 describe(`Event Manager unit tests`, () => {
 
@@ -89,6 +117,29 @@ describe(`Event Manager unit tests`, () => {
         expect(act).not.toThrow(Error)
     })
 
+    it('listeners should be called if an event dispatched', () => {
+        const eventManager = new EventManager({
+            events: { events: [{ event: 'eventName', listeners: [ new MockedListener ] }] }
+        })
+
+        eventManager.init()
+
+        eventManager.dispatch('eventName')
+
+        expect(listenerCalled).toBe(true)
+    })
+
+    it('listeners should be able accept optional payloads', () => {
+        const eventManager = new EventManager({
+            events: { events: [{ event: 'eventName', listeners: [ new MockedListener ] }] }
+        })
+
+        eventManager.init()
+
+        eventManager.dispatch('eventName', mockedPayload)
+
+        expect(listenerPayload).toBe(mockedPayload)
+    })
+
     // TODO should contains init, dispatch
-    // TODO tests for dispatching and listeners
 })
