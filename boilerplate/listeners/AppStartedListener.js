eba7f79ba5eec3f1cbad23e4612edcc9d1d25fda
Mehran Hadidi <mehran.hadidi@gmail.com>
2020-01-13 10:25:26 +0300

    20 Events/tests (#8)
    * 20: add must-check for config, add init check before dispatching
    
    * 20: add unit tests for EventManager
    
    * 20: add payload test, refactor test descritions
    
    * 20: add feature tests for event manager
    
    * 20: move feature test to unit test file
    
    * 20: allow event manager to handle async, promises & non-promises listeners + tests
    
    * 20: starting point for dynamic injection
    
    * 20: cleaned up
    
    Co-authored-by: Alexandr Shevchenko <ls.shov@gmail.com>

---
+++ boilerplate/listeners/AppStartedListener.js
@@ -0,0 +1,14 @@
+const ListenerInterface = toweran.ListenerInterface
+
+class AfterAppStarted extends ListenerInterface {
+  handle() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('application started')
+        resolve()
+      }, 3000)
+    })
+  }
+}
+
+module.exports = AfterAppStarted
