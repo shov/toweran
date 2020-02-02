498d151b193636e805854f8a5531d26e16ddff37
Mikhail Snitko <mikhail.for.rs@gmail.com>
2020-02-02 19:34:26 +0300

    test: Add cutting test cases for windows.

--- tests/unit/lib/DependencyInjector/CuttingPatternTest.js
+++ tests/unit/lib/DependencyInjector/CuttingPatternTest.js
@@ -1,6 +1,7 @@
 'use strict'
 
 require('../../../bootstrap')
+const { sep } = require('path'); // system path separator
 
 describe(`Dependency Injector pattern cutting`, () => {
   const DependencyInjectorOrigin = require(toweran.FRAMEWORK_PATH + '/lib/DependencyInjector')
@@ -49,10 +50,59 @@ describe(`Dependency Injector pattern cutting`, () => {
     ]
   }
 
-  dataProvider().forEach((set, index) => {
-    it(`#${index}, ${set.comment}`, () => {
-      expect(diStub.cutPattern(set.pattern)).toBe(set.result)
-    })
-  })
+  function windowsDataProvider() {
+    return [
+      {
+        comment: 'dir',
+        pattern: 'D:\\a\\b\\c\\root\\app',
+        result: 'D:/a/b/c/root/app',
+      },
+      {
+        comment: 'dir with trailing slash',
+        pattern: 'd:\\a\\b\\c\\root\\app\\',
+        result: 'd:/a/b/c/root/app',
+      },
+      {
+        comment: 'root dir',
+        pattern: 'd:\\a',
+        result: 'd:/a',
+      },
+      {
+        comment: 'pattern simple',
+        pattern: 'd:\\a\\b\\c\\root/app/*',
+        result: 'd:/a/b/c/root/app',
+      },
+      {
+        comment: 'pattern double',
+        pattern: 'd:\\a\\b\\c\\root\\app/**/*',
+        result: 'd:/a/b/c/root/app',
+      },
+      {
+        comment: 'pattern mixed',
+        pattern: 'c:\\a\\b\\c\\root/lib/provider*/**/*',
+        result: 'c:/a/b/c/root/lib',
+      },
+      {
+        comment: 'pattern question mixed',
+        pattern: 'd:\\a\\b\\c\\root\\adapters/http?/*',
+        result: 'd:/a/b/c/root/adapters',
+      },
+    ]
+  }
 
+  if (sep === '/') {
+    dataProvider().forEach((set, index) => {
+      it(`#${index}, ${set.comment}`, () => {
+        expect(diStub.cutPattern(set.pattern)).toBe(set.result)
+      })
+    })
+  } else if (sep === '\\') {
+    windowsDataProvider().forEach((set, index) => {
+      it(`#${index}, ${set.comment}`, () => {
+        expect(diStub.cutPattern(set.pattern)).toBe(set.result)
+      })
+    })
+  } else {
+    throw new Error(`Unknown system path separator "${sep}"!`)
+  }
 })
