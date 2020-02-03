bd22fd0c87a048c35f58ca3e830c8317e47d693a
Mikhail Snitko <mikhail.for.rs@gmail.com>
2020-02-03 04:40:12 +0300

    feat: Export toweran namespace instead define global variable in toweran.js file.

--- toweran.js
+++ toweran.js
@@ -2,61 +2,200 @@
 
 //TODO: Global NS, contracts, exceptions, must. Make sure everything is here and remove the todo
 //TODO: make PATH constants immutable if not testing env
-const path = require('path')
+
+const FRAMEWORK_PATH = __dirname
+let must = null
 
 /**
- * Toweran namespace
- * @type {{}}
+ * @property {string} FRAMEWORK_PATH - Path to the framework root
+ * @property {Object} C - Constants
+ * @property {Must} must - Object that helps to validate
+ *
+ * @property {App} App - The application constructor
+ *
+ * @property {InvalidArgumentException} InvalidArgumentException - Constructor
+ * @property {ResourceNotFoundException} ResourceNotFoundException - Constructor
+ *
+ * @property {ContainerInterface} ContainerInterface - Interface
+ * @property {ListenerInterface} ListenerInterface - Interface
+ * @property {ContainerRegistrationInterface} ContainerRegistrationInterface - Interface
+ * @property {LoggerInterface} LoggerInterface - Interface
+ *
+ * @property {BasicServiceProvider} BasicServiceProvider - Constructor
+ *
+ * @property {HelperServiceProvider} HelperServiceProvider - Constructor
+ * @property {ConfigReader} ConfigReader - Constructor
+ * @property {DependencyInjectionServiceProvider} DependencyInjectionServiceProvider - Constructor
+ * @property {Logger} Logger - Constructor
+ * @property {EventServiceProvider} EventServiceProvider - Constructor
+ * @property {Container} Container - Constructor
+ * @property {ContainerRegistration} ContainerRegistration - Constructor
+ *
+ * @type {{
+ *   FRAMEWORK_PATH: string,
+ *   App: App,
+ *   C: {},
+ *   must: Must,
+ *   HelperServiceProvider: HelperServiceProvider,
+ *   ConfigReader: ConfigReader,
+ *   DependencyInjectionServiceProvider: DependencyInjectionServiceProvider,
+ *   ListenerInterface: ListenerInterface,
+ *   Logger: Logger,
+ *   ContainerRegistrationInterface: ContainerRegistrationInterface,
+ *   BasicServiceProvider: BasicServiceProvider,
+ *   EventServiceProvider: EventServiceProvider,
+ *   Container: Container,
+ *   InvalidArgumentException: InvalidArgumentException,
+ *   ContainerInterface: ContainerInterface,
+ *   ResourceNotFoundException: ResourceNotFoundException,
+ *   LoggerInterface: LoggerInterface,
+ *   ContainerRegistration: ContainerRegistration
+ * }}
  */
-global.toweran = {}
+const toweran = {
+  /**
+   * Path to the framework root.
+   * @type {string}
+   */
+  FRAMEWORK_PATH,
 
+  /**
+   * Constants
+   */
+  get C() {
+    return require(FRAMEWORK_PATH + '/lib/constants')
+  },
 
-toweran.FRAMEWORK_PATH = path.resolve(__dirname)
-const FRAMEWORK_PATH = toweran.FRAMEWORK_PATH
+  /**
+   * Object that helps to validate
+   * @return {Must}
+   */
+  get must() {
+    if (!must) {
+      must = new (require(FRAMEWORK_PATH + '/lib/Must'))
+    }
+    return must
+  },
 
-/**
- * Constants
- */
-toweran.C = require(FRAMEWORK_PATH + '/lib/constants')
+  /**
+   * @return {InvalidArgumentException}
+   * @constructor
+   */
+  get InvalidArgumentException() {
+    return require(FRAMEWORK_PATH + '/lib/exceptions/InvalidArgumentException')
+  },
 
-/**
- * Exceptions
- */
-toweran.InvalidArgumentException = require(FRAMEWORK_PATH + '/lib/exceptions/InvalidArgumentException')
-toweran.ResourceNotFoundException = require(FRAMEWORK_PATH + '/lib/exceptions/ResourceNotFoundException')
+  /**
+   * @return {ResourceNotFoundException}
+   * @constructor
+   */
+  get ResourceNotFoundException() {
+    return require(FRAMEWORK_PATH + '/lib/exceptions/ResourceNotFoundException')
+  },
 
-/**
- * Object that helps to validate
- * @type {Must}
- */
-toweran.must = new (require(FRAMEWORK_PATH + '/lib/Must'))
+  /**
+   * @return {LoggerInterface}
+   * @constructor
+   */
+  get LoggerInterface() {
+    return require(FRAMEWORK_PATH + '/lib/contracts/LoggerInterface')
+  },
 
-/**
- * Contracts: Interfaces & Basic classes
- */
-toweran.LoggerInterface = require(FRAMEWORK_PATH + '/lib/contracts/LoggerInterface')
-toweran.ContainerRegistrationInterface = require(FRAMEWORK_PATH + '/lib/contracts/ContainerRegistrationInterface')
-toweran.ContainerInterface = require(FRAMEWORK_PATH + '/lib/contracts/ContainerInterface')
-toweran.ListenerInterface = require(FRAMEWORK_PATH + '/lib/contracts/ListenerInterface')
+  /**
+   * @return {ContainerRegistrationInterface}
+   * @constructor
+   */
+  get ContainerRegistrationInterface() {
+    return require(FRAMEWORK_PATH + '/lib/contracts/ContainerRegistrationInterface')
+  },
 
-toweran.BasicServiceProvider = require(FRAMEWORK_PATH + '/lib/contracts/BasicServiceProvider')
+  /**
+   * @return {ContainerInterface}
+   * @constructor
+   */
+  get ContainerInterface() {
+    return require(FRAMEWORK_PATH + '/lib/contracts/ContainerInterface')
+  },
 
-/**
- * Core constructors
- */
-toweran.Logger = require(FRAMEWORK_PATH + '/lib/Logger')
-toweran.ContainerRegistration = require(FRAMEWORK_PATH + '/lib/ContainerRegistration')
-toweran.Container = require(FRAMEWORK_PATH + '/lib/Container')
-toweran.ConfigReader = require(FRAMEWORK_PATH + '/lib/ConfigReader')
+  /**
+   * @return {ListenerInterface}
+   * @constructor
+   */
+  get ListenerInterface() {
+    return require(FRAMEWORK_PATH + '/lib/contracts/ListenerInterface')
+  },
 
-/**
- * Core service providers
- */
-toweran.HelperServiceProvider = require(FRAMEWORK_PATH + '/lib/serviceProviders/HelperServiceProvider')
-toweran.DependencyInjectionServiceProvider = require(FRAMEWORK_PATH + '/lib/serviceProviders/DependencyInjectionServiceProvider')
-toweran.EventServiceProvider = require(FRAMEWORK_PATH + '/lib/serviceProviders/EventServiceProvider')
+  /**
+   * @return {BasicServiceProvider}
+   * @constructor
+   */
+  get BasicServiceProvider() {
+    return require(FRAMEWORK_PATH + '/lib/contracts/BasicServiceProvider')
+  },
 
-/**
- * The App
- */
-toweran.App = require(FRAMEWORK_PATH + '/lib/App')
+  /**
+   * @return {Logger}
+   * @constructor
+   */
+  get Logger() {
+    return require(FRAMEWORK_PATH + '/lib/Logger')
+  },
+
+  /**
+   * @return {ContainerRegistration}
+   * @constructor
+   */
+  get ContainerRegistration() {
+    return require(FRAMEWORK_PATH + '/lib/ContainerRegistration')
+  },
+
+  /**
+   * @return {Container}
+   * @constructor
+   */
+  get Container() {
+    return require(FRAMEWORK_PATH + '/lib/Container')
+  },
+
+  /**
+   * @return {ConfigReader}
+   * @constructor
+   */
+  get ConfigReader() {
+    return require(FRAMEWORK_PATH + '/lib/ConfigReader')
+  },
+
+  /**
+   * @return {HelperServiceProvider}
+   * @constructor
+   */
+  get HelperServiceProvider() {
+    return require(FRAMEWORK_PATH + '/lib/serviceProviders/HelperServiceProvider')
+  },
+
+  /**
+   * @return {DependencyInjectionServiceProvider}
+   * @constructor
+   */
+  get DependencyInjectionServiceProvider() {
+    return require(FRAMEWORK_PATH + '/lib/serviceProviders/DependencyInjectionServiceProvider')
+  },
+
+  /**
+   * @return {EventServiceProvider}
+   * @constructor
+   */
+  get EventServiceProvider() {
+    return require(FRAMEWORK_PATH + '/lib/serviceProviders/EventServiceProvider')
+  },
+
+  /**
+   * @return {App}
+   * @constructor
+   */
+  get App() {
+    return require(FRAMEWORK_PATH + '/lib/App')
+  },
+}
+
+module.exports = toweran
