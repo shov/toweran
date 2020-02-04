35f197fbe091dbdeea86302c495f82b58f503e8c
Alexandr Shevchenko <ls.shov@gmail.com>
2020-02-05 00:18:29 +0300

    28,30: changed routes definition a bit, working on rotesResolver

--- boilerplate/routes/api.js
+++ boilerplate/routes/api.js
@@ -1,8 +1,20 @@
 'use strict'
+const {
+  C
+} = toweran
 
 module.exports = [
-  {
+  {//TODO: add naming for routes
     path: '/api/v1',
+    method: 'get',
+    resolver: (req, res, next) => {
+
+      //TODO: make it easier
+      const protocol = `http${process.env.SSL === 'true' ? 's' : ''}://`
+      const url = `${protocol}${req.hostname}/api/v1/welcome`
+
+      res.redirect(C.HTTP.MOVED_PERMANENTLY, url)
+    },
     sub: [
       {
         path: '/welcome',
